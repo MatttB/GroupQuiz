2c4d684e0d974c4b129ce02f134c87e530986863
lemattb <lemattb@gmail.com>
2014-12-05 00:53:12 +0000

    * code cleanup * UI improvements * made db update calls on next and previous button presses - expensive? idk

--- public/modules/quizzes/controllers/quizzes.client.controller.js
+++ public/modules/quizzes/controllers/quizzes.client.controller.js
@@ -4,7 +4,6 @@
 angular.module('quizzes').controller('QuizzesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Quizzes',
 	function($scope, $stateParams, $location, Authentication, Quizzes, Question ) {
 		$scope.authentication = Authentication;
-
 		// Create new Quiz
 		$scope.create = function() {
 			// Create new Quiz object
@@ -21,9 +20,7 @@ angular.module('quizzes').controller('QuizzesController', ['$scope', '$statePara
 				}],
 				settings: {}
 			});
-/*
 
-*/
 			// Redirect after save
 			quiz.$save(function(response) {
 				$location.path('quizzes/' + response._id + '/edit');
@@ -51,17 +48,6 @@ angular.module('quizzes').controller('QuizzesController', ['$scope', '$statePara
 			}
 		};
 
-		// Update existing Quiz
-		$scope.update = function() {
-			var quiz = $scope.quiz ;
-			//console.log(quiz);
-			quiz.$update(function() {
-				$location.path('quizzes/' + quiz._id + '/edit');
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-
 		// Find a list of Quizzes
 		$scope.find = function() {
 			$scope.quizzes = Quizzes.query();
@@ -69,7 +55,7 @@ angular.module('quizzes').controller('QuizzesController', ['$scope', '$statePara
 
 		// Find existing Quiz
 		$scope.findOne = function() {
-			$scope.quiz = Quizzes.get({ 
+			$scope.quiz = Quizzes.get({
 				quizId: $stateParams.quizId
 			});
 		};
@@ -77,30 +63,6 @@ angular.module('quizzes').controller('QuizzesController', ['$scope', '$statePara
 		/*
 			Questions page
 		 */
-
-		$scope.tempQuestion = {
-			'title': '',
-			'hint': '',
-			'answer': [],
-			'wrongAnswers': []
-		};
-
-		//$scope.appendQuestion = function() {
-			/*
-			 var question = new Question({
-			 title: this.title,
-			 hint: this.hint,
-			 answer: this.answer,
-			 wrongAnswer: this.wrongAnswer
-			 });
-			 */
-			//var quiz = $scope.quiz;
-			//quiz.questions.push($scope.tempQuestion);
-
-			//console.log('test');
-			//$scope.setNumPages();
-			//console.log(quiz.questions.length);
-		//};
 
 		$scope.addAnswer = function(){
 			var quiz = $scope.quiz;
@@ -122,9 +84,21 @@ angular.module('quizzes').controller('QuizzesController', ['$scope', '$statePara
 			quiz.questions[$scope.currentPage-1].wrongAnswers.pop();
 		};
 
+
+		// Update existing Quiz
+		$scope.update = function() {
+			var quiz = $scope.quiz;
+
+			quiz.$update(function () {
+				$location.path('quizzes/' + quiz._id + '/edit');
+				$scope.error = undefined;
+			}, function (errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
 		$scope.updateQuestion = function() {
 			var quiz = $scope.quiz;
-			console.log(quiz);
 
 			var pushQuestion = function(){
 				quiz.questions.push({
@@ -135,49 +109,29 @@ angular.module('quizzes').controller('QuizzesController', ['$scope', '$statePara
 				});
 			};
 
-			var popQuestion = function(){
-				quiz.questions.pop();
-			};
-
 			quiz.$update(function () {
 				$location.path('quizzes/' + quiz._id + '/edit');
 				pushQuestion();
 				$scope.error = undefined;
-				$scope.currentPage = $scope.currentPage + 1;
-				$scope.numPages = $scope.numPages + 1;
-				$scope.showDelAndPrev = true;
+				$scope.currentPage++;
+				$scope.numPages++;
 			},function(errorResponse) {
 					console.log(errorResponse);
-					if($scope.quiz.questions.length === 1){
-						$scope.showDelAndPrev = false;
-					}
-					//popQuestion();
 					$scope.error = errorResponse.data.message;
 				}
 			);
 		};
 
-		$scope.showDelAndPrev = false;
-
 		$scope.delQuestion = function(){
 			if($scope.currentPage === $scope.quiz.questions.length){
+				$scope.quiz.questions.pop();
 				$scope.currentPage --;
 			}
-			$scope.quiz.questions.splice($scope.currentPage - 1, 1);
+			else {
+				$scope.quiz.questions.splice($scope.currentPage - 1, 1);
+			}
+			$scope.update();
 			$scope.numPages --;
-			//current page button visibility clean-up
-			if($scope.quiz.questions.length === $scope.currentPage){
-				$scope.showAdd = true;
-				if($scope.currentPage === 1){
-					$scope.showDelAndPrev = false;
-				}
-				else{
-					$scope.showDelAndPrev = true;
-				}
-			}
-			else{
-				$scope.showAdd = false;
-			}
 		};
 
 		$scope.nextQuestion = function() {
@@ -187,52 +141,15 @@ angular.module('quizzes').controller('QuizzesController', ['$scope', '$statePara
 			}
 			else {
 				$scope.currentPage++;
-				$scope.showDelAndPrev = true;
+				$scope.update();
 			}
 		};
 
 		$scope.prevQuestion = function(){
 			$scope.currentPage --;
+			$scope.update();
 		};
 
-		/*
-		console.log('@');
-		console.log($scope.error);
-		if(!$scope.error){
-			if ($scope.currentPage === $scope.numPages) {
-				quiz.questions.push({
-					title: '',
-					hint: '',
-					answer: [],
-					wrongAnswers: []
-				});
-				$scope.numPages = quiz.questions.length;
-				$scope.currentPage = $scope.currentPage + n;
-			}
-			$scope.currentPage = $scope.currentPage + 1;
-		}
-*/
-		$scope.formatNumber = function(){
-			if( ($scope.currentPage > $scope.quiz.questions.length) || ($scope.currentPage < 1) ) {
-				$scope.currentPage = $scope.quiz.questions.length;
-			}
-		};
-
-		$scope.updateNumPages = function() {
-			$scope.numPages = $scope.quiz.questions.length;
-		};
-
-		//$scope.setNumPages();
-		//$scope.numPages = $scope.FindNumPages();
-		//$scope.SetNumPages();
-		/*
-		$scope.$watch('$scope.quiz.questions.length', function(){
-				$scope.SetNumPages();
-			}
-		);
-		*/
-		//$scope.numPages = $scope.quiz.questions.length;
-		//$scope.numPages = $scope.quiz.questions.length;
 		$scope.numPages = 1;
 		$scope.currentPage = 1;
 		$scope.maxSize = 10;
